efc14eb556b9cab67861ff71c1025763ab0d3e87
Bruno Araújo <brunoluan@gmail.com>
2015-07-30 09:35:10 -0300

    Install Collective\Html

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,9 @@ class HomeController extends Controller {
     {
         $clientes = Cliente::orderBy('nome','ASC')->get();
 
-        dd($clientes);
+        echo '<pre>';
+        var_dump($clientes);
+        exit;
 
 //        return View::make('home.index')->with('clientes',$clientes);
 //
