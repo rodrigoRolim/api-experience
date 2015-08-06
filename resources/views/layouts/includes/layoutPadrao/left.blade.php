f5241806d752c4723925e4348b7cbd36bcb2ea85
Vitor <vitorvqz@gmail.com>
2015-08-06 13:17:38 -0300

    Definicao Layout Padrao

---
+++ resources/views/layouts/includes/layoutPadrao/left.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar-default navbar-static-side" role="navigation">
+    <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 100%;"><div class="sidebar-collapse" style="overflow: hidden; width: auto; height: 100%;">
+        <ul class="nav metismenu" id="side-menu">
+            <li class="nav-header">
+                FILTRO	                        
+            </li>
+            
+            <li>
+                <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Graphs</span><span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level collapse">
+                    <li><a href="graph_flot.html">Flot Charts</a></li>
+                    <li><a href="graph_morris.html">Morris.js Charts</a></li>
+                    <li><a href="graph_rickshaw.html">Rickshaw Charts</a></li>
+                    <li><a href="graph_chartjs.html">Chart.js</a></li>
+                    <li><a href="graph_chartist.html">Chartist</a></li>
+                    <li><a href="graph_peity.html">Peity Charts</a></li>
+                    <li><a href="graph_sparkline.html">Sparkline Charts</a></li>
+                </ul>
+            </li>
+            <li class="active">
+                <a href="mailbox.html"><i class="fa fa-envelope"></i> <span class="nav-label">Mailbox </span><span class="label label-warning pull-right">16/24</span></a>
+                <ul class="nav nav-second-level collapse">
+                    <li><a href="mailbox.html">Inbox</a></li>
+                    <li><a href="mail_detail.html">Email view</a></li>
+                    <li><a href="mail_compose.html">Compose email</a></li>
+                    <li><a href="email_template.html">Email templates</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="metrics.html"><i class="fa fa-pie-chart"></i> <span class="nav-label">Metrics</span> <span class="label label-primary pull-right">NEW</span> </a>
+            </li>
+            <li>
+                <a href="widgets.html"><i class="fa fa-flask"></i> <span class="nav-label">Widgets</span></a>
+            </li>
+
+        </ul>
+    </div><div class="slimScrollBar" style="width: 7px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 381.225px; background: rgb(0, 0, 0);"></div><div class="slimScrollRail" style="width: 7px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; opacity: 0.9; z-index: 90; right: 1px; background: rgb(51, 51, 51);"></div></div>
+</nav>
---
+++ resources/views/layouts/layoutPadrao.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<title></title>
+
+	{!! Html::style('/assets/css/bootstrap.min.css') !!}
+
+    {!! Html::style('/assets/css/animate.css') !!} 
+	{!! Html::style('/assets/css/inspinia.css') !!}
+	{!! Html::style('/assets/css/custom.css') !!}
+	{!! Html::style('/assets/css/skins/red.css') !!} 
+	
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+	@section('stylesheets')
+		
+	@show
+</head>
+
+<body class="gray-bg boxed-layout bodyLayoutPadrao">
+	<nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+      	<header id="header">
+			<nav class="navbar navbar-static-top headLogin headPadrao" role="navigation" style="margin-bottom: 0">
+				<div class="navbar-header">
+		          	{!! Html::image('/assets/images/logo.png', 'SmartAdmin', array('title' => 'logo')) !!}  
+		        </div>
+				
+				<div id="navbar" class="navbar-collapse collapse text-right">
+		        	@yield('infoHead')
+		        </div>
+		        
+		        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		            <span class="sr-only">Toggle navigation</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		          </button>
+			</nav> 	
+		</header>
+      </div>
+    </nav>
+
+    <div class="container">
+    	<div id="wrapper">
+    		@include('layouts.includes.layoutPadrao.left')
+	        <div id="page-wrapper" class="gray-bg" style="min-height: 356px;">
+            <div class="row border-bottom">
+            <nav class="navbar navbar-static-top  " role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+            </div>
+
+            <ul class="nav navbar-top-links navbar-right">
+                <li>
+                    <span class="m-r-sm text-muted welcome-message">
+                        @yield('infoAtendimento')
+                    </span>
+                </li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">&nbsp;</a>
+                </li>
+            </ul>
+        </nav>
+        </div>
+            <div class="row wrapper border-bottom white-bg page-heading">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="ibox">                       
+                                <ul class="sortable-list connectList agile-list ui-sortable">
+                                    <li class="warning-element col-md-6">
+                                       <b>GLI</b> | GLICEMIA EM JEJUM
+                                        <div class="agile-detail">                                              
+                                            <i></i> Em Andamento
+                                        </div>
+                                    </li>
+                                    <li class="success-element col-md-6">
+                                        <b> TSH </b> | TIREOESTIMULANTE HORMONIO (TSH)
+                                        <div class="agile-detail">                                              
+                                            <i></i> Finalizado
+                                        </div>
+                                    </li>                                       
+                                    <li class="danger-element col-md-6">
+                                        All the Lorem Ipsum generators
+                                        <div class="agile-detail">
+                                            <a href="#" class="pull-right btn btn-xs btn-primary">Done</a>
+                                            <i class="fa fa-clock-o"></i> 06.10.2015
+                                        </div>
+                                    </li>
+                                     <li class="warning-element col-md-6">
+                                       <b>GLI</b> | GLICEMIA EM JEJUM
+                                        <div class="agile-detail">                                              
+                                            <i></i> Em Andamento
+                                        </div>
+                                    </li>
+                                </ul>                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer">
+                <div class="pull-right">
+                    10GB of <strong>250GB</strong> Free.
+                </div>
+                <div>
+                    <strong>Copyright</strong> Example Company © 2014-2015
+                </div>
+            </div>
+        </div>
+    	</div>
+    </div>
+
+	<script src="{{ asset('/assets/js/jquery-2.1.1.js') }}"></script>
+	<script src="{{ asset('/assets/js/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('/assets/js/plugins/jasny/jasny-bootstrap.min.js') }}"></script>
+	<script src="{{ asset('/assets/js/plugins/datapicker/bootstrap-datepicker.js') }}"></script>
+	<script src="{{ asset('/assets/js/plugins/iCheck/icheck.min.js') }}"></script>
+	
+	@section('script')
+
+	@show
+</body>
+</html>
