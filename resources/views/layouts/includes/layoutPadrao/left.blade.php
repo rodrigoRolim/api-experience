a7b7778016f2173a5eb0bf0d85271297fae48af9
Vitor <vitorvqz@gmail.com>
2015-08-19 11:56:48 -0300

    Ajustes Layout Mobile

--- resources/views/layouts/includes/layoutPadrao/left.blade.php
+++ resources/views/layouts/includes/layoutPadrao/left.blade.php
@@ -16,7 +16,6 @@
                 </li>
             @endforeach
         </ul>
-    </div><div class="slimScrollBar" style="width: 7px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 381.225px; background: rgb(0, 0, 0);"></div><div class="slimScrollRail" style="width: 7px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; opacity: 0.9; z-index: 90; right: 1px; background: rgb(51, 51, 51);"></div></div>
 </nav>
 
 @section('script')
