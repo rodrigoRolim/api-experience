cb90a63dbc7588ecb1787aad322ee59ced8c9dec
Bruno Araújo <brunoluan@gmail.com>
2015-12-02 11:51:45 -0300

    Ajuste logo view 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -42,7 +42,7 @@
     <body>
         <div class="container">
             <div class="content">
-            	{!! Html::image('/assets/images/logo_g.png', 'Codemed', array('title' => 'logo', 'style' => 'height:120px')) !!}
+                {!! Html::image(config('system.clienteLogo'), 'logo_lab', array('title' => 'logo')) !!}
                 <div class="title">Página não encontrada</div>
             </div>
         </div>
