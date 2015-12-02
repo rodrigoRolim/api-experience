470c5649b8e93749a21790558f78b5d1ff93e7e6
Bruno Araújo <brunoluan@gmail.com>
2015-12-02 11:53:27 -0300

    Logo experience 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -42,7 +42,7 @@
     <body>
         <div class="container">
             <div class="content">
-                {!! Html::image(config('system.clienteLogo'), 'logo_lab', array('title' => 'logo')) !!}
+                {!! Html::image(config('system.eXperienceLogoHorizontal'), 'logo_exp', array('title' => 'eXperience - codemed', 'style'=>'height: 100px;')) !!}
                 <div class="title">Página não encontrada</div>
             </div>
         </div>
