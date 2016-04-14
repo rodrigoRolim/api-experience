86a33ccb8ee7b4d3ce61d8777ae4c9af06514503
Bruno Araújo <brunoluan@gmail.com>
2016-04-14 12:00:18 -0300

    Correcao de conf

--- resources/views/auth/autoatendimento.blade.php
+++ resources/views/auth/autoatendimento.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @if (count($errors) == 1)
     <div class="alert alert-danger alert-dismissable">
         @foreach ($errors->all() as $error)
@@ -11,5 +10,3 @@
 	{!! Form::hidden('id','eyJwb3N0byI6ICIwMCIsImF0ZW5kaW1lbnRvIjogIjAwMTkyNyIsInNlbmhhIiA6ICJMMVVLSEwifQ==', array('id'=>'id')) !!}
     <button type="submit" class="btn btn-primary block full-width m-b">Acessar</button>
 {!! Form::close() !!}
-=======
->>>>>>> efe50a9c27e5a33dd7058d042cedaaef56d7f53d
